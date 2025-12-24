62e8b6ccb1a6d1bf63aa0a8d2c00d2b15aacaddd
sangiii1725 <bodasangeeth1997@gmail.com>
2025-12-24 17:28:43 +0530

    resolved syntax errors

--- Basic_RectangleClass.cpp
+++ Basic_RectangleClass.cpp
@@ -8,10 +8,10 @@ class rectangle{
     public:
         rectangle(){length = 1.0, breadth = 1.0;}
         rectangle(double L, double B){if(L<0){cout<<"given length is lessthen zero, so making it default"; length = 1.0;}else{length = L;} if(B<0){cout<<"given breadth is less then zero, s making it default"; breadth = 1.0;}else{breadth = B;}}
-        rectangle(rectangle& rect){this->length = rect.length; this->breadth = rect.breadth}
+        rectangle(rectangle& rect){this->length = rect.length; this->breadth = rect.breadth;}
 
-        
-}
+
+};
 
 int main(){
     return 0;
