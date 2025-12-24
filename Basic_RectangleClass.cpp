2ceae400c9a9198c899fb2ff9d57f4df76fde298
sangiii1725 <bodasangeeth1997@gmail.com>
2025-12-24 17:27:17 +0530

    added different types of constructors

--- Basic_RectangleClass.cpp
+++ Basic_RectangleClass.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+class rectangle{
+    private:
+        double length;
+        double breadth;
+    public:
+        rectangle(){length = 1.0, breadth = 1.0;}
+        rectangle(double L, double B){if(L<0){cout<<"given length is lessthen zero, so making it default"; length = 1.0;}else{length = L;} if(B<0){cout<<"given breadth is less then zero, s making it default"; breadth = 1.0;}else{breadth = B;}}
+        rectangle(rectangle& rect){this->length = rect.length; this->breadth = rect.breadth}
+
+        
+}
+
 int main(){
     return 0;
 }
