b9ec8878aa6d170edf2baa8e916a2a13ebd8cafa
santhoshkumar <bodasanthosh.kumar@tcs.com>
2025-12-25 09:49:20 +0530

    DeepcopyConstructor

---
+++ DeepcopyConstructor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class example{
+    private:
+        double* ptr = new double(20.5);
+    public:
+        example(){*ptr = 0;}
+        example(int d){*ptr = d;}
+        example(example& ex){*ptr = *ex.ptr;} // Deep copy constructor
+
+        int get_data(){return *ptr;}
+        void set_data(int d){*ptr = d;}
+
+        void display(){cout<<"Data: "<<get_data()<<endl;}
+
+        ~example(){cout<<"example object destructed"<<endl;}
+};
+
+int main(){
+
+    example E1(42);
+    example* E2;
+    E2 = &E1;
+    cout<<E2->get_data()<<endl;
+
+    example* E3;
+    E3 = new example(E1); // Using deep copy constructor
+
+    E3->display();
+
+    delete E3;
+    E3 = nullptr;
+
+    return 0;
+}
